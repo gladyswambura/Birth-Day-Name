1981c1cae48d756a6cfad95b8e803796d5c5e0eb
gladyswambura <gladyswahito7@gmail.com>
2022-03-04 20:54:17 +0300

    creates a responsive navigation bar with the help of bootstrap

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+$("#lightswitch i").click(function () {
+    var isDark = $("body").is(".dark");
+    $("body").toggleClass("dark", !isDark);
+    $(this).toggleClass("fa-moon", isDark).toggleClass("fa-sun", !isDark);
+});
