fd20a0fcb6c0349506577ff75e8e039e5cacefd0
gladyswambura <gladyswahito7@gmail.com>
2022-03-06 11:44:52 +0300

    Bump: bumps arrays in our document.js to increase the version of our form

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,7 @@
 function showText() {
     alert("Great start! now click OK and -scroll down to fill the form")
 }
+// arrays.
+let mAkanName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let fAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
