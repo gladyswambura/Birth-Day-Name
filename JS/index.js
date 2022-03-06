719a81ba5cbc7dcc46995cf3daea9721d4188f02
gladyswambura <gladyswahito7@gmail.com>
2022-03-06 12:09:07 +0300

    optimizes my form by getting substrings of the century, year, month, day count starts from zero

--- JS/index.js
+++ JS/index.js
@@ -10,3 +10,18 @@ const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "
 // get date from user
 function button1() {
     var date = document.getElementById('DOB').value;
+
+    // (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) mod 7
+
+    // CC - is the century digits.For example 1989 has CC = 19
+    // YY - is the Year digits(1989 has YY = 89)
+    // MM - is the Month
+    // DD - is the Day of the month
+    // mod - is the modulus
+    // function ( % )
+
+     // example user inputs 2022-12-31
+     var CC = date.substring(0, 2);
+     var YY = date.substring(2, 4);
+     var MM = date.substring(5, 7);
+     var DD = date.substring(8, 10);
