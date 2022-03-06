702d3c6b19b2ffa92fa6321a7ecaa517048b0034
gladyswambura <gladyswahito7@gmail.com>
2022-03-06 12:06:06 +0300

    increases the performance of the function button1() which gets date from the user

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,7 @@ function showText() {
 let mAkanName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let fAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+// get date from user
+function button1() {
+    var date = document.getElementById('DOB').value;
